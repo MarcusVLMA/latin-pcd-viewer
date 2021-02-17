94d7b0922527fdd4447cefd9cf452e5cae43fe6d
MarcusVLMA <marcusviniciuslma@gmail.com>
2021-02-17 18:22:40 -0300

    Updating README.md

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Simple Web PCD files Viewer made with Three JS
 Run a `npm install` on root folder and in public/js/pcd-viewer to install all dependencies.
 
 To run the application, run `nodemon app.js` in root folder.
+
+**Obs:** This web app have `nosetip_finder.node` as a dependency. You can check the [Nosetip Finder repository](https://github.com/MarcusVLMA/nosetip_finder/) for more info.
--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.post('/find-nosetip', (req, res, next) => {
           return;
         }
         let response = nosetip_finder.findNoseTip(file.file.path);
-        console.log("CHAMA NA REPSONSE", response)
+        
         fs.unlink(file.file.path, (error) => {
           if(error) throw error;
           
@@ -33,34 +33,5 @@ app.post('/find-nosetip', (req, res, next) => {
         });
       });
 });
-
-// app.post('/teste', (req, res, next) => {
-//   const form = formidable({ multiples: false });
-    
-//   form.on('fileBegin', (name, file) => {
-//       file.path = `./uploaded_files/zip_files/${file.name}`;
-//   });
-
-//   form.parse(req, (err, fields, file) => {
-//       if (err) {
-//         next(err);
-//         return;
-//       }
-//       console.log(file)
-//       var new_zip = new JSZip();
-      
-//       new_zip.loadAsync(file.file.path)
-//       .then(function(zip) {
-//           console.log(zip)
-//       });
-
-//       // fs.unlink(file.file.path, (error) => {
-//       //   if(error) throw error;
-        
-//       //   res.json({skr: 'skr'});
-//       // });
-//     });
-// });
-
 
 app.listen(process.env.PORT || 3000);
