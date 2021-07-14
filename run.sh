298f935cb430ba546b09c0228f7c657b6aa411f7
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-07-14 14:21:29 -0300

    add default filter configs

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-cp ../nosetip_finder/cmd/nan-module/build/Release/nosetip_finder.node . && npm run dev
+cp ../nosetip_finder/cmd/nan-module/build/Release/fiducial_point_finder.node ./modules/ &&
+cp ../nosetip_finder/cmd/nan-module-pipeline/build/Release/pipeline.node ./modules/ &&
+cp ../point-explorer/nan-module/build/Release/point_explorer.node ./modules/ &&
+npm run dev
