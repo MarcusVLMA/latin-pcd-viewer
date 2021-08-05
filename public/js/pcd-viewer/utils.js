61c34762b74d82b782a8a58fb86e4baaada4749d
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-08-05 17:22:33 -0300

    fix generate color issues

--- public/js/pcd-viewer/utils.js
+++ public/js/pcd-viewer/utils.js
@@ -1,5 +1,5 @@
 export function getRandomColorAndSize() {
-    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    const randomColor = `#${(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0')}`;
     const randomSize = Math.round(10 * (Math.random() * (1.6 - 0.8) + 0.8)) / 10;
 
     return [randomColor, randomSize];
